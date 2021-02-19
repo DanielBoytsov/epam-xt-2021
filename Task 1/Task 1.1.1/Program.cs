a9199f7508b02b26dfe466837813b10f6dd87322
Daniel Boytsov <danil.boytsov6@gmail.com>
2021-02-19 17:09:55 +0400

    Added Task 1.1.1

---
+++ Task 1/Task 1.1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            do
+            {
+                Console.WriteLine("Put the length of the rectangle here");
+                if ((!double.TryParse(Console.ReadLine(), out a))|(a <= 0))
+                {
+                    Console.WriteLine("Put the correct value!");
+                }
+            } while (a <= 0);
+            do
+            {
+                Console.WriteLine("Put the weidth of the rectangle here");
+                if ((!double.TryParse(Console.ReadLine(), out b))|(b <= 0))
+                {
+                    Console.WriteLine("Put the correct value!");
+                }
+            } while (b <= 0);
+            double s = a * b;
+            Console.WriteLine($"Square of your rectangle = {s}");
+        }
+    }
+}
