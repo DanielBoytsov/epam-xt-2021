90113d42becb7c911a9e660afdd78ae9abec3c0c
Daniel Boytsov <danil.boytsov6@gmail.com>
2021-04-26 18:36:31 +0400

    Fixed 3.3.2

---
+++ Task 3/Task 3.3.2/AdditionalString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_3._3._2
+{
+    internal static class AdditionalString
+    {
+        public static string LanguageCheck(this string text)
+        {
+            if (text is not null && text != String.Empty)
+            {
+                string[] splitText = text.Split(new char[] { ',', '.', '!', '?', ' ', ':', ';',
+                    '[', ']', '"', '{', '}', '-', '\n', '\r', '(', ')' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (splitText.Length == 1)
+                {
+
+                    if (Regex.IsMatch(splitText[0], @"^\d+$"))
+                    {
+                        return "Numbers";
+                    }
+
+                    if (Regex.IsMatch(splitText[0], @"^[А-ЯЁа-яё]+$"))
+                    {
+                        return "Russian";
+                    }
+
+                    if (Regex.IsMatch(splitText[0], @"^[A-z]+$"))
+                    {
+                        return "English";
+                    }
+                    if (Regex.IsMatch(splitText[0], @"(\d|[A-z]|[А-Яа-я])+"))
+                    {
+                        return "Mixed";
+                    }
+                }
+
+                else
+                {
+
+                    {
+                        return "Mixed";
+                    }
+                }
+            }
+            throw new NullReferenceException(message: "Your reference is null, or you put an empty string");
+        }
+    }
+}
--- Task 3/Task 3.3.2/Program.cs
+++ Task 3/Task 3.3.2/Program.cs
@@ -8,52 +8,11 @@ namespace Task_3._3._2
     {
         static void Main(string[] args)
         {
-            string test = "Daniel";
+            Console.WriteLine("Put here the string, which you want to check.");
+            string test = Console.ReadLine();
 
             Console.WriteLine(test.LanguageCheck());
         }
     }
-    internal static class AdditionalString
-    {
-        public static string LanguageCheck(this string text)
-        {
-            if (text is not null && text != String.Empty)
-            {
-                string[] splitText = text.Split(new char[] { ',', '.', '!', '?', ' ', ':', ';', '[', ']', '"', '{', '}', '-', '\n', '\r', '(', ')' },
-                    StringSplitOptions.RemoveEmptyEntries);
-
-                if (splitText.Length == 1)
-                {
-
-                    if (Regex.IsMatch(splitText[0], @"([0-9]){0,}"))
-                    {
-                        return "Numbers";
-                    }
-
-                    if (Regex.IsMatch(splitText[0], @"([А-ЯЁа-яё]){0,}"))
-                    {
-                        return "Russian";
-                    }
-
-                    if (Regex.IsMatch(splitText[0], @"([A-z]){0,}"))
-                    {
-                        return "English";
-                    }
-                    if (Regex.IsMatch(splitText[0], @"([А - ЯЁа - яё][A - Za - z]\d){0,}"))
-                    {
-                        return "Mixed";
-                    }
-                }
-
-                else
-                {
-
-                    {
-                        return "Mixed";
-                    }
-                }
-            }
-            throw new NullReferenceException(message: "Your reference is null, or you put an empty string");
-        }
-    }
+    
 }
